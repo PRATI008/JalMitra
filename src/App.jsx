aa7ae71a4e9c4b406009c4561729dcca71cfc107
Shriyamishra22 <mishrashriya495@gmail.com>
2026-09-16 18:09:36 +0530

    Resolved App.jsx merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,20 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 import Home from './Pages/Home'
-<<<<<<< HEAD
 import Splash from './Pages/Splash'
 import Welcome from './Pages/Welcome'
-=======
 import Payment from './Pages/Payment'
 import MyProfile from './Pages/MyProfile'
->>>>>>> 35c96d63e683824597257f62e4d2139ecbbf7987
 
 const App = () => {
   return (
     <Router>
       <Routes>
-
-<<<<<<< HEAD
-<Route path='/' element={<Splash/>} />
-<Route path='/welcome' element={<Welcome/>} />
-
-<Route path='/home' element={<Home/>} />
-=======
-        <Route path='/' element={<Home />} />
->>>>>>> 35c96d63e683824597257f62e4d2139ecbbf7987
-
+        <Route path='/' element={<Splash />} />
+        <Route path='/welcome' element={<Welcome />} />
+        <Route path='/home' element={<Home />} />
         <Route path='/payment' element={<Payment />} />
-
         <Route path='/myprofile' element={<MyProfile />} />
-
       </Routes>
     </Router>
   )
