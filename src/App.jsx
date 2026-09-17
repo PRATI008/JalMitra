8453412ad3662c8b3a0cfdee46db0ff5d36eab0c
shalu-1-2 <shalukushwaha356@gmail.com>
2026-09-17 14:20:13 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,7 @@ import Home from './Pages/Home'
 import Splash from './Pages/Splash'
 import Welcome from './Pages/Welcome'
 import MyProfile from './Pages/MyProfile'
-<<<<<<< HEAD
 import TrackOrder from './Pages/TrackOrder'
-
-const App = () => {
-  return (
-   <>
-   <Router>
-    <Routes>
-
-<Route path='/' element={<Home/>} />
-<Route path='/my-profile' element={<MyProfile/>} />
-<Route path='/trackorder'   element={<TrackOrder/>}/>
-    </Routes>
-   </Router>
-   </>
-=======
 import SearchBar from './Pages/SearchBar'
 import SupplierDetails from './Pages/SupplierDetails'
 
@@ -31,12 +16,13 @@ const App = () => {
         <Route path='/' element={<Splash />} />
         <Route path='/welcome' element={<Welcome />} />
         <Route path='/home' element={<Home />} />
+        <Route path='/my-profile' element={<MyProfile />} />
         <Route path='/myprofile' element={<MyProfile />} />
+        <Route path='/trackorder' element={<TrackOrder />} />
         <Route path='/search-bar' element={<SearchBar />} />
         <Route path='/supplier/:id' element={<SupplierDetails />} />
       </Routes>
     </Router>
->>>>>>> 9a996a9fe11bdcc6467b5136c45c973ad3cc9caf
   )
 }
 
